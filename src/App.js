11927f879fb7cc4084797bb8be8a467f08111217
ithespv <ithespv@gmail.com>
2019-02-11 13:41:05 +0200

    Edit screen width

--- src/App.js
+++ src/App.js
@@ -6,49 +6,97 @@ class App extends Component {
   constructor () {
     super()
 
+    this.appRef = React.createRef()
+
     this.state = {
       blocks: [
         {
           x: 0,
-          y: 1,
+          y: 5,
         },
         {
           x: 0,
           y: 0,
         },
         {
-          x: -1,
-          y: 0,
-        },
-        {
-          x: -2,
-          y: 0,
-        },
-        {
-          x: -3,
-          y: 0,
-        },
-        {
-          x: -3,
-          y: -1,
-        },
-        {
           x: 0,
-          y: -1,
+          y: -5,
         },
-      ]
+        {
+          x: 5,
+          y: 0,
+        },
+        {
+          x: -5,
+          y: 0,
+        },
+        // {
+        //   x: -1,
+        //   y: 0,
+        // },
+        // {
+        //   x: -2,
+        //   y: 0,
+        // },
+        // {
+        //   x: -3,
+        //   y: 0,
+        // },
+        // {
+        //   x: -3,
+        //   y: -1,
+        // },
+      ],
+      extremePositions: {
+        right: 5,
+        bottom: 5,
+      }
     }
   }
 
-  addBlock = (nextCoordinates) => {
-    const prevBlocks = this.state.blocks
+  addBlock = nextCoordinates => {
+    const {
+      blocks,
+      extremePositions,
+    } = this.state
+
+    const nextExtreme = {
+      right: extremePositions.right < Math.abs(nextCoordinates.x) ? Math.abs(nextCoordinates.x) : extremePositions.right,
+      bottom: extremePositions.bottom < Math.abs(nextCoordinates.y) ? Math.abs(nextCoordinates.y) : extremePositions.bottom,
+    }
+
+    this.updateSize(nextExtreme)
 
     this.setState({
       blocks: [
-        ...prevBlocks,
+        ...blocks,
         nextCoordinates,
-      ]
+      ],
+      extremePositions: {
+        ...extremePositions,
+        ...nextExtreme,
+      }
     })
+  }
+
+  updateSize = extremePositions => {
+    const blockHeight = 100
+    const blockWidth = 100
+
+    const width = (extremePositions.right * 2 + 2) * blockWidth
+    const height = (extremePositions.bottom * 2 + 2) * blockHeight
+
+    console.log({
+      width: `${width}px`,
+      height: `${height}px`,
+    })
+
+    this.appRef.current.style.width = `${width}px`
+    this.appRef.current.style.height = `${height}px`
+  }
+
+  componentDidMount() {
+    this.updateSize(this.state.extremePositions)
   }
 
   render() {
@@ -57,7 +105,7 @@ class App extends Component {
     } = this.state
 
     return (
-      <div className="App">
+      <div className="App" ref={this.appRef}>
         {
           blocks.map((block, idx) =>
             <Block coordinates={block} key={idx} handler={this.addBlock} />
