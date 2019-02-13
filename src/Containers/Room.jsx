239a3cd0d634527a758ced66e41a6ea14fe38ff3
ithespv <ithespv@gmail.com>
2019-02-13 13:12:57 +0200

    add room views

---
+++ src/Containers/Room.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+
+})
+
+class Room extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Room)
--- src/Views/Room.jsx
+++ src/Views/Room.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Block = ({ image }) =>
+const Room = () =>
   <div>
     
   </div>
 
-export default Block
+export default Room
