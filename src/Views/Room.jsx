dd573f216e7356e6aae22e481145dc51df11bd55
ithespv <ithespv@gmail.com>
2019-03-10 19:23:04 +0200

    add exits view

--- src/Views/Room.jsx
+++ src/Views/Room.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Room = ({ coordinates, exits, margins, getExit, addRoom, image }) => {
+const Room = ({ coordinates, exits, exitsAmount, margins, getExit, addRoom, image }) => {
   return <div className="block" style={{
     ...margins,
     background: `url(${image})`
   }}>
+    <span>amount: {exitsAmount}<br/>
+    camount: {exits.length}
+    </span>
     {
       exits && exits.map(exit =>
         <div className={`block__side ${getExit(exit).className}`}
